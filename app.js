ac21d512bd7d0b7549ad36e25634aa8fe5020b91
vivek <vivekshelke12jul@gmail.com>
2022-08-29 02:33:48 +0530

    vivek ka pehla commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/random', (req, res) => {
+    console.log('random route accesed')
+    const num = Math.floor(Math.random()*10)
+    console.log(num)
+    res.status(200).json({info: num})
+})
+
+
+app.listen(3000, console.log('server started'))
---
+++ public/frontend.js
@@ -0,0 +1,14 @@
+console.log('frontendjs connected')
+
+const num = document.querySelector('#num')
+const getBtn = document.querySelector('#getBtn')
+getBtn.addEventListener('click', getInfo)
+
+const url = '/random'
+async function getInfo(e){
+    e.preventDefault()
+    const res = await fetch(url,{method: 'GET'})
+    const data = await res.json();
+    console.log(data.info);
+    num.innerHTML = data.info
+}
